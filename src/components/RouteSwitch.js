8bee7cef77cc4c614423cc7d8d41bc3a9bfd1ff8
hattima-tim <irfanridwan20@gmail.com>
2022-08-07 08:53:35 +0000

    add  a route for halfSleeveCutTShirts component

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
+import HalfSleeveCutTShirts from "./products/halfSleeveCutTShirts";
 
 function RouteSwitch() {
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-        </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/half-sleeve-cut-and-sew-solid"
+          element={<HalfSleeveCutTShirts />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default RouteSwitch;
