70c74070ebb6b6c71ff533817a229f2a1fae619d
hattima-tim <irfanridwan20@gmail.com>
2022-08-07 16:26:15 +0000

    add tests for half sleeve t shirts home page

---
+++ src/tests/halfSleeveHomePage.test.js
@@ -0,0 +1,17 @@
+import { render,screen } from "@testing-library/react";
+import HalfSleeveHomePage from "../components/products/halfSleeveTShirts/main";
+import {MemoryRouter} from "react-router-dom";
+
+test("renders with correct texts", () => {
+    const {container} = render(<HalfSleeveHomePage />, { wrapper: MemoryRouter });
+    expect(container).toMatchSnapshot();
+});
+
+test('images are rendered', () => {
+    render(<HalfSleeveHomePage />, {wrapper: MemoryRouter});
+    const images = screen.getAllByRole('img');
+    expect(images.length).toBe(6);
+    for (let i = 0; i < images.length; i++){
+        expect(images[i]).toBeVisible();
+    }
+})
