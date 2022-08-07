975f4613c7f3fa7abada1f5a23519d3ea7bc862c
hattima-tim <irfanridwan20@gmail.com>
2022-08-07 08:52:53 +0000

    add breadcrumbs to halfSleeveCutTShirts

---
+++ src/components/products/halfSleeveCutTShirts.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import '../../styles/productsPage.css';
+const HalfSleeveCutTShirts = () => {
+    return (
+        <div className='breadCrumbs'>
+           <Link to='/'>Home</Link> / Half Sleeve Cut T-Shirts
+        </div>
+    )
+}
+
+export default HalfSleeveCutTShirts;
