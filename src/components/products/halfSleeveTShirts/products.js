bf5c75a4d322fa4ed05c95bf627223ea77a34998
hattima-tim <irfanridwan20@gmail.com>
2022-08-07 15:47:00 +0000

    added halfSleeve products

---
+++ src/components/products/halfSleeveTShirts/products.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+      img: "https://res.cloudinary.com/du3oueesv/image/upload/v1659873489/shopping%20cart/half%20sleeve%20cut/mthcssp10-solid-350x435_zvwuma.jpg",
+      name: "Half Sleeve Cut and Sew Solid(pattern 15)",
+      price: "$450",
+    },
+    {
+      img: "https://res.cloudinary.com/du3oueesv/image/upload/v1659873473/shopping%20cart/half%20sleeve%20cut/mthcssp11-solid-350x435_jooe3k.jpg",
+      name: "Half Sleeve Cut and Sew Solid(pattern 14)",
+      price: "$450",
+    },
+    {
+      img: "https://res.cloudinary.com/du3oueesv/image/upload/v1659873453/shopping%20cart/half%20sleeve%20cut/mthcssp12-solid-350x435_zkricr.jpg",
+      name: "Half Sleeve Cut and Sew Solid(pattern 13)",
+      price: "$450",
+    },
+    {
+      img: "https://res.cloudinary.com/du3oueesv/image/upload/v1659873413/shopping%20cart/half%20sleeve%20cut/mthcssp13-solid-350x435_zutbr0.jpg",
+      name: "Half Sleeve Cut and Sew Solid(pattern 12)",
+      price: "$450",
+    },
+    {
+      img: "https://res.cloudinary.com/du3oueesv/image/upload/v1659873393/shopping%20cart/half%20sleeve%20cut/mthcssp14-solid-350x435_sc3l07.jpg",
+      name: "Half Sleeve Cut and Sew Solid(pattern 11)",
+      price: "$450",
+    },
+    {
+      img: "https://res.cloudinary.com/du3oueesv/image/upload/v1659873363/shopping%20cart/half%20sleeve%20cut/mthcssp15-solid-350x435_mz8sws.jpg",
+      name: "Half Sleeve Cut and Sew Solid(pattern 10)",
+      price: "$450",
+    },
+  ];
+
+  export default products;
