44271ab1855255ebf72c647f154529f8e0474098
hattima-tim <irfanridwan20@gmail.com>
2022-08-09 12:06:53 +0000

    add route for rendering individual product page

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import HalfSleeveCutTShirts from "./products/halfSleeveTShirts/main";
+import { Product } from "./products/productPage";
+import ProductPage from './products/productPage';
 
 function RouteSwitch() {
   return (
@@ -11,6 +13,9 @@ function RouteSwitch() {
           path="/half-sleeve-cut-and-sew-solid"
           element={<HalfSleeveCutTShirts />}
         />
+        <Route path='/product' element={<Product/>}>
+          <Route path=":name" element={<ProductPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
