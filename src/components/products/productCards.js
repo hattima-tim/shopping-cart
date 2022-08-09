824ceea81cecfdaf30f902907be3936a059eeb25
hattima-tim <irfanridwan20@gmail.com>
2022-08-09 16:07:30 +0000

    nest productCard image with react router Link
    this will help users to go the product page by clicking the product image

--- src/components/products/productCards.js
+++ src/components/products/productCards.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function ProductCards({ products }) {
   return (
     <div className="productCards">
       {products.map((product, index) => (
         <div className="productCard" key={index}>
-          <div className="productImg">
-            <img src={product.img} alt={product.name} />
+          <div className="productCardImg">
+            <Link to={`/product/${product.pathName}`}>
+              <img src={product.img} alt={product.name} />
+            </Link>
           </div>
           <div className="productCard__info">
             <h3>{product.name}</h3>
