79ec9ef7986feb850b336c8dace9c5a7ae1a2acc
hattima-tim <irfanridwan20@gmail.com>
2022-08-10 04:11:41 +0000

    add breadCrumbs in productPage

--- src/components/products/productPage.js
+++ src/components/products/productPage.js
@@ -1,5 +1,7 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useParams, Link } from "react-router-dom";
+import uniqid from "uniqid";
 import getProductData from "./halfSleeveTShirts/productsData";
+import "../../styles/productPage.css";
 
 export function Product() {
   return (
@@ -17,6 +19,25 @@ function ProductPage() {
     <div>
       <div className="main">
         <img className="productImage" src={productData.img} alt={productData.name} />
+        <div className="mainInfo">
+          <div className="breadcrumbs">
+            {productData.breadCrumbs.map((breadCrumb, index) => {
+              if (index === productData.breadCrumbs.length - 1) {
+                return (
+                  <Link key={uniqid()} to={breadCrumb.path}>
+                    {breadCrumb.name}
+                  </Link>
+                );
+              } else {
+                return (
+                  <Link key={uniqid()} to={breadCrumb.path}>
+                    {breadCrumb.name + "/"}
+                  </Link>
+                );
+              }
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
