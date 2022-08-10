5f1100e5beba0d48866f3460cdff3d77847701cb
hattima-tim <irfanridwan20@gmail.com>
2022-08-10 03:24:02 +0000

    add product image in product page

--- src/components/products/productPage.js
+++ src/components/products/productPage.js
@@ -1,4 +1,6 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
+import getProductData from "./halfSleeveTShirts/productsData";
+
 export function Product() {
   return (
     <>
@@ -6,3 +8,18 @@ export function Product() {
     </>
   );
 }
+
+function ProductPage() {
+
+  const params = useParams();
+  const productData = getProductData(params.name);
+  return (
+    <div>
+      <div className="main">
+        <img className="productImage" src={productData.img} alt={productData.name} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
