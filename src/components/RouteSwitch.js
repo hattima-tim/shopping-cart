610d3be56d4176e1dc4b95511702a067ff5794c6
hattima-tim <irfanridwan20@gmail.com>
2022-08-11 13:47:54 +0000

    nest all other routes inside header

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -2,19 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import HalfSleeveCutTShirts from "./products/halfSleeveTShirts/main";
 import { Product } from "./products/productPage";
-import ProductPage from './products/productPage';
+import ProductPage from "./products/productPage";
+import Header from "./header";
 
 function RouteSwitch() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/half-sleeve-cut-and-sew-solid"
-          element={<HalfSleeveCutTShirts />}
-        />
-        <Route path='/product' element={<Product/>}>
-          <Route path=":name" element={<ProductPage />} />
+        <Route path="/" element={<Header />}>
+          <Route index element={<App />} />
+          <Route
+            path="/half-sleeve-cut-and-sew-solid"
+            element={<HalfSleeveCutTShirts />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":name" element={<ProductPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
