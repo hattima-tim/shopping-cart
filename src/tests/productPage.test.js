e2402914af71f03cfee6234424e8af874ae138ed
hattima-tim <irfanridwan20@gmail.com>
2022-08-15 14:00:23 +0000

    add tests for description and additional info of product page

--- src/tests/productPage.test.js
+++ src/tests/productPage.test.js
@@ -54,3 +54,61 @@ describe("item counter", () => {
     expect(inputField.value).toBe("56");
   });
 });
+
+describe("description and additional info", () => {
+  test('additional info is shown when "additional info" button is clicked', () => {
+    setup();
+    const additionalInfoBtn = screen.getByRole("button", {
+      name: "Additional Information",
+    });
+    userEvent.click(additionalInfoBtn);
+
+    const additionalInfoTables = screen.getAllByRole("table");
+    expect(additionalInfoTables.length).toBe(1);
+
+    const tableData1 = screen.getByRole("cell", { name: "0.13kg" });
+    expect(tableData1).toBeInTheDocument();
+
+    const tableData2 = screen.getByRole("cell", { name: "COMBED COTTON" });
+    expect(tableData2).toBeInTheDocument();
+
+    const tableData3 = screen.getByRole("cell", { name: "M, L, XL, XXL" });
+    expect(tableData3).toBeInTheDocument();
+  });
+
+  test("description is shown when description tab is clicked", () => {
+    setup();
+
+    const additionalInfoBtn = screen.getByRole("button", {
+      name: "Additional Information",
+    });
+    const descriptionBtn = screen.getByRole("button", {
+      name: "Description",
+    });
+
+    userEvent.click(additionalInfoBtn);
+    userEvent.click(descriptionBtn);
+
+    const para = screen.getByText("Size Measurement (in inch/centimeter):");
+
+    expect(para).toBeInTheDocument();
+
+    const listItem1 = screen.getByText(
+      "M(Medium): Chest 39.37″ inch/ 100 cm, Height 27.55″ inch/ 70 cm"
+    );
+    const listItem2 = screen.getByText(
+      "L(large) : Chest 40.94″inch/ 104 cm, Height 28.34″inch/ 72 cm"
+    );
+    const listItem3 = screen.getByText(
+      "XL(Extra large): Chest 42.51″ inch/ 108 cm, Height 29.13″ inch/ 74 cm"
+    );
+    const listItem4 = screen.getByText(
+      "XXL(Extra large) : Chest 44.09″ inch/ 112 cm, Height 29.92″ inch/ 76 cm"
+    );
+
+    expect(listItem1).toBeInTheDocument();
+    expect(listItem2).toBeInTheDocument();
+    expect(listItem3).toBeInTheDocument();
+    expect(listItem4).toBeInTheDocument();
+  });
+});
