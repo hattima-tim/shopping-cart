5ed3efc74c68cb35cd91579d971da8466125b9b4
hattima-tim <irfanridwan20@gmail.com>
2022-08-31 09:10:12 +0000

    style product cards for small devices

--- src/components/products/productCards.js
+++ src/components/products/productCards.js
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 
 function ProductCards({ products }) {
   return (
-    <div className="productCards">
+    <div className="productCards ">
       {products.map((product, index) => (
-        <div className="productCard" key={index}>
+        <div className="productCard w-40 mb-7" key={index}>
           <div className="productCardImg">
             <Link to={`/product/${product.pathName}`}>
-              <img src={product.img} alt={product.name} />
+              <img src={product.img} alt={product.name}/>
             </Link>
           </div>
-          <div className="productCard__info">
+          <div className="productCard__info text-sm pt-2 text-[#565656]">
             <h3>{product.name}</h3>
-            <p>{product.price}</p>
+            <p className="font-bold text-black">{product.price}</p>
           </div>
         </div>
       ))}
