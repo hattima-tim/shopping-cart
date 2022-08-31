c0b6658a8618ed637810b2316bbeee5c802d360a
hattima-tim <irfanridwan20@gmail.com>
2022-08-31 13:50:27 +0000

    add breadcrumb style for large devices

--- src/components/products/halfSleeveTShirts/main.js
+++ src/components/products/halfSleeveTShirts/main.js
@@ -3,14 +3,14 @@ import ProductCards from "../productCards";
 import products from "./products";
 
 import "../../../styles/productsPage.css";
-import '../../../styles/productCards.css';
+import "../../../styles/productCards.css";
 
 const HalfSleeveCutTShirts = () => {
-
   return (
     <>
-      <div className="text-xl font-normal text-center my-4">
-        <Link to="/">HOME /</Link><span className="text-black">Half Sleeve Cut T-Shirts</span>
+      <div className="my-4 text-center text-xl font-normal lg:ml-20 lg:text-left">
+        <Link to="/">HOME /</Link>
+        <span className="text-black">Half Sleeve Cut T-Shirts</span>
       </div>
       <ProductCards products={products} />
     </>
