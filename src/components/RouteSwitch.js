b6971fdb397e8d370e455e4cc450a00c3e490f16
hattima-tim <irfanridwan20@gmail.com>
2022-09-05 08:50:35 +0000

    make change to have clean database management
    if the old persisted, then I would have to put all products data in a single file.
    Now with the current change, it is possible to put
    products data in different file

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import HalfSleeveCutTShirts from "./products/halfSleeveTShirts/main";
-import { Product } from "./products/productPage";
-import ProductPage from "./products/productPage";
+import { Product } from "./ProductPage";
+import ProductPage from "./ProductPage";
 import Header from "./header";
 import Cart from "./cart";
+import getHalfSleeveCutTShirt from "./products/halfSleeveTShirts/productsData";
 
 function RouteSwitch() {
   return (
@@ -12,13 +13,22 @@ function RouteSwitch() {
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<App />} />
+
           <Route
             path="/half-sleeve-cut-and-sew-solid"
             element={<HalfSleeveCutTShirts />}
           />
-          <Route path="/product" element={<Product />}>
-            <Route path=":name" element={<ProductPage />} />
+
+          <Route
+            path="/half-sleeve-cut-and-sew-solid/product"
+            element={<Product />}
+          >
+            <Route
+              path=":name"
+              element={<ProductPage getProductData={getHalfSleeveCutTShirt} />}
+            />
           </Route>
+
           <Route path="/cart" element={<Cart />} />
         </Route>
       </Routes>
