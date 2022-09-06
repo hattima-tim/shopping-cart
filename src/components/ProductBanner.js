8ebe91144402d53254a9aa7fbfad34825d91ecb9
hattima-tim <irfanridwan20@gmail.com>
2022-09-06 16:03:06 +0000

    show 'VIEW COLLECTION' btn on condition

--- src/components/ProductBanner.js
+++ src/components/ProductBanner.js
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
 
-const ProductBanner = ({ src, alt, productCategoryHomePageLink }) => {
+const ProductBanner = ({
+  src,
+  alt,
+  productCategoryHomePageLink = null,
+  showViewCollectionButton = true,
+}) => {
   return (
     <div className="relative my-4 mx-4 lg:mx-20">
       <img className="" src={src} alt={alt}></img>
 
-      <Link
-        to={productCategoryHomePageLink}
-        className="absolute top-1/2 left-1/2 mt-4 -translate-x-1/2 transform border-2 border-black py-1 px-4 text-[.5rem] font-bold text-black md:text-xs md:font-medium lg:text-xs lg:font-medium"
-      >
-        VIEW COLLECTION {"> >"}
-      </Link>
+      {showViewCollectionButton && productCategoryHomePageLink !== null && (
+        <Link
+          to={productCategoryHomePageLink}
+          className="absolute top-1/2 left-1/2 mt-4 -translate-x-1/2 transform border-2 border-black py-1 px-4 text-[.5rem] font-bold text-black md:text-xs md:font-medium lg:text-xs lg:font-medium"
+        >
+          VIEW COLLECTION {"> >"}
+        </Link>
+      )}
     </div>
   );
 };
