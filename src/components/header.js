0f85fa4965d4d1485589b58e442c298b8c7b4a55
hattima-tim <irfanridwan20@gmail.com>
2022-09-07 13:35:36 +0000

    add style to cart icon area and product tooltip

--- src/components/header.js
+++ src/components/header.js
@@ -38,23 +38,23 @@ function Header() {
           ></img>
         </Link>
 
-        <div className="shopping-cart-icon py-0 px-1">
-          <span className="text-sm self-center text-black">৳ {subTotal}.00</span>
+        <div className="shopping-cart-icon py-1 px-2 cursor-pointer">
+          <span id='subTotal' className="text-sm self-center text-black">৳ {subTotal}.00</span>
 
           <span className="fa-layers fa-fw">
-            <svg className="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <svg className="w-5 m-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
               <path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z" />
             </svg>
             <span
-              className="fa-layers-counter"
-              style={{ background: "Tomato" }}
+              className="fa-layers-counter font-bold top-[-12px] left-[13px]"
+              style={{ background: "black" }}
               data-testid="cart-item-count"
             >
               {totalItem}
             </span>
           </span>
 
-          <div className="products_tooltip">
+          <div className="products_tooltip cursor-auto">
             {productsInCart.toString() !== "" && (
               <>
                 <div className="products_container">
@@ -67,10 +67,10 @@ function Header() {
 
                         <div className="product_tooltip_item_info">
                           <div className="tooltip_header">
-                            <h3>{product.name}</h3>
+                            <h3 className="text-[#4e4e4e]">{product.name}</h3>
                             <button
                               data-id={index}
-                              className="remove_item_btn"
+                              className="remove_item_btn p-2"
                               onClick={removeItemFromCart}
                             >
                               x
@@ -80,16 +80,16 @@ function Header() {
                           <p>FABRIC: {product.fabric}</p>
                           <p>SIZE: {product.size}</p>
                           <p>
-                            {product.quantity} x {product.price}
+                            {product.quantity} x <span className="font-bold">{product.price}</span>
                           </p>
                         </div>
                       </div>
                     );
                   })}
                 </div>
-                <p className="subTotal">
+                <p className="flex justify-center py-3 border-t border-t-[#dcdcdc] border-b-2 border-b-[#dcdcdc]">
                   <span>Subtotal: ৳</span>
-                  {subTotal}
+                  <span className="text-black">{subTotal}</span>
                 </p>
 
                 <Link to="/cart" className="view_cart">
