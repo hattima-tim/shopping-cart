9b97a0e257e91a9846e47332832ee8809a2f3e8f
hattima-tim <irfanridwan20@gmail.com>
2022-09-08 05:55:32 +0000

    make header sticky and not sticky on condition

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "../styles/header.css";
 function Header() {
@@ -27,9 +27,19 @@ function Header() {
     setProductsInCart([]);
   };
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        document.querySelector(".header").classList.remove("fixed");
+      } else {
+        document.querySelector(".header").classList.add("fixed");
+      }
+    });
+    observer.observe(document.querySelector("#top-nav-bar"));
+  });
   return (
     <>
-      <div className="header bg-[#f0f0f0] fixed z-10 w-full">
+      <div className="header z-10 w-full bg-[#f0f0f0]">
         <Link to="/" className="my-3">
           <img
             src="https://res.cloudinary.com/du3oueesv/image/upload/v1660122044/shopping%20cart/oubd-logo-with-tm-black-1-1400x642_se0t70.png"
@@ -38,15 +48,21 @@ function Header() {
           ></img>
         </Link>
 
-        <div className="shopping-cart-icon py-1 px-2 cursor-pointer">
-          <span id='subTotal' className="text-sm self-center text-black">৳ {subTotal}.00</span>
+        <div className="shopping-cart-icon cursor-pointer py-1 px-2">
+          <span id="subTotal" className="self-center text-sm text-black">
+            ৳ {subTotal}.00
+          </span>
 
           <span className="fa-layers fa-fw">
-            <svg className="w-5 m-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <svg
+              className="m-0 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
               <path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z" />
             </svg>
             <span
-              className="fa-layers-counter font-bold top-[-12px] left-[13px]"
+              className="fa-layers-counter top-[-12px] left-[13px] font-bold"
               style={{ background: "black" }}
               data-testid="cart-item-count"
             >
@@ -80,14 +96,15 @@ function Header() {
                           <p>FABRIC: {product.fabric}</p>
                           <p>SIZE: {product.size}</p>
                           <p>
-                            {product.quantity} x <span className="font-bold">{product.price}</span>
+                            {product.quantity} x{" "}
+                            <span className="font-bold">{product.price}</span>
                           </p>
                         </div>
                       </div>
                     );
                   })}
                 </div>
-                <p className="flex justify-center py-3 border-t border-t-[#dcdcdc] border-b-2 border-b-[#dcdcdc]">
+                <p className="flex justify-center border-t border-b-2 border-t-[#dcdcdc] border-b-[#dcdcdc] py-3">
                   <span>Subtotal: ৳</span>
                   <span className="text-black">{subTotal}</span>
                 </p>
