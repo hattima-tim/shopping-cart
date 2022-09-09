05185d37e8a11be638bdf5546e0bacc4987a4271
hattima-tim <irfanridwan20@gmail.com>
2022-09-09 05:24:44 +0000

    add sideNav icon

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,6 +3,20 @@ import "../styles/sidenav.css";
 function SideNav() {
   return (
     <>
+      <div
+        onClick={() => {
+
+        }}
+        className="group cursor-pointer rounded-full border border-gray-500 p-3 hover:bg-black"
+      >
+        <svg
+          className="m-[.1rem] w-3 fill-gray-500 group-hover:fill-white"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
+      </div>
     </>
   );
 }
