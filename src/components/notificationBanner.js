25a7dd4a5056b02f007ef9d30bc0151fcf94a8b6
hattima-tim <irfanridwan20@gmail.com>
2022-09-17 13:13:12 +0000

    change width of the notificationBanner

--- src/components/notificationBanner.js
+++ src/components/notificationBanner.js
@@ -11,7 +11,7 @@ function NotificationBanner({
   return (
     <>
       {resultOfUserAction !== "" && (
-        <div className="status-update align-center mt-4 mx-auto flex w-11/12 items-center justify-between gap-5 border-l-4 border-l-green-600 bg-white p-2 md:pl-12">
+        <div className="status-update align-center mt-4 flex w-full items-center justify-between gap-5 border-l-4 border-l-green-600 bg-white p-2 md:pl-12">
           <p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" />
@@ -36,7 +36,10 @@ function NotificationBanner({
             <p>Your cart is currently empty.</p>
           </div>
 
-          <Link to='/' className="mx-auto my-2 block bg-black py-2 px-4 text-base font-semibold text-white w-fit hover:text-white">
+          <Link
+            to="/"
+            className="mx-auto my-2 block w-fit bg-black py-2 px-4 text-base font-semibold text-white hover:text-white"
+          >
             RETURN TO SHOP
           </Link>
         </>
