599d90c9c9363fc59c2923a0e3c5c063a50d0141
hattima-tim <irfanridwan20@gmail.com>
2023-06-01 17:21:04 +0000

    add type for ProductBanner component's props

--- src/components/ProductBanner.tsx
+++ src/components/ProductBanner.tsx
@@ -1,33 +1,32 @@
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+
+type ProductBannerProps = {
+  src: string;
+  alt: string;
+  productCategoryHomePageLink?: string | null;
+  showViewCollectionButton?: boolean;
+};
 
 const ProductBanner = ({
   src,
   alt,
   productCategoryHomePageLink = null,
   showViewCollectionButton = true,
-}) => {
+}: ProductBannerProps) => {
   return (
-    <div className="relative my-4 mx-4 lg:mx-20">
+    <div className="relative mx-4 my-4 lg:mx-20">
       <img className="" src={src} alt={alt}></img>
 
       {showViewCollectionButton && productCategoryHomePageLink !== null && (
         <Link
           to={productCategoryHomePageLink}
-          className="absolute top-1/2 left-1/2 mt-4 -translate-x-1/2 transform border-2 border-black py-1 px-4 text-[.5rem] font-bold text-black md:text-xs md:font-medium lg:text-xs lg:font-medium"
+          className="absolute left-1/2 top-1/2 mt-4 -translate-x-1/2 transform border-2 border-black px-4 py-1 text-[.5rem] font-bold text-black md:text-xs md:font-medium lg:text-xs lg:font-medium"
         >
           VIEW COLLECTION {"> >"}
         </Link>
       )}
     </div>
   );
-};
-
-ProductBanner.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-  productCategoryHomePageLink: PropTypes.string,
-  showViewCollectionButton: PropTypes.bool,
 };
 
 export default ProductBanner;
