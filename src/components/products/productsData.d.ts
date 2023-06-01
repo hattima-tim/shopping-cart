2ae94a9e24dff48740aed7803d5de4603a717984
hattima-tim <irfanridwan20@gmail.com>
2023-06-01 14:00:32 +0000

    create an interface for Product

---
+++ src/components/products/productsData.d.ts
@@ -0,0 +1,19 @@
+export interface Product {
+  breadCrumbs: Array<{ name: string; path: string }>;
+  pathName: string;
+  fullPath: string;
+  imagesForProductPage: string[];
+  imgForProductCard: string;
+  name: string;
+  price: string;
+  details: string[];
+  fabric: string[];
+  color: string[];
+  size: string[];
+  description: string[];
+  additionalInfo: {
+    weight: string;
+    fabric?: string;
+    size: string;
+  };
+}
