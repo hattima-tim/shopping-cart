120c9d4e01529f256df9425ef2ebbbe945fcb924
hattima-tim <irfanridwan20@gmail.com>
2023-06-02 18:15:23 +0000

    create CartProductsDataType interface

---
+++ src/components/types/productsInCart.d.ts
@@ -0,0 +1,13 @@
+export interface CartProductsDataType {
+  id: string;
+  name: string;
+  img: string;
+  price: string;
+  quantity: number;
+  subTotal: number;
+  fabric: string;
+  color: string;
+  size: string;
+  path: string;
+  fullPath: string;
+}
