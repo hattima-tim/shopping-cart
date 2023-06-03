6e04ebf511c3891e93b85bd3fa49f68f34a70ada
hattima-tim <irfanridwan20@gmail.com>
2023-06-03 02:35:51 +0000

    add null check for root element

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import RouteSwitch from './components/RouteSwitch';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import RouteSwitch from "./components/RouteSwitch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root not found.");
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <RouteSwitch />
