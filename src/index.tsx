32cb68ae390751cb618332a47bc61ffca990af5e
hattima-tim <irfanridwan20@gmail.com>
2023-06-05 17:18:36 +0000

    provide redux store to the application components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import RouteSwitch from "./components/RouteSwitch";
+import store from "./app/store";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root not found.");
@@ -9,6 +11,8 @@ if (!rootElement) throw new Error("Root not found.");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <Provider store={store}>
+      <RouteSwitch />
+    </Provider>
   </React.StrictMode>
 );
