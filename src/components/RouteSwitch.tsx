59fd87a1c146548f2f277a2fc4d509672ab76ed4
hattima-tim <irfanridwan20@gmail.com>
2023-06-06 06:59:24 +0000

    change route logic to not use outletContext

--- src/components/RouteSwitch.tsx
+++ src/components/RouteSwitch.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import ProductCategoryHomePage from "./ProductCategoryHome";
-import { Product } from "./ProductPage";
 import ProductPage from "./ProductPage";
 import Header from "./header";
 import Footer from "./Footer";
@@ -18,121 +17,79 @@ import { getSpecificPoloTShirtData } from "./products/poloTShirts/productsData";
 function RouteSwitch() {
   return (
     <BrowserRouter basename="/shopping-cart">
+      <Header />
       <Routes>
-        <Route path="/" element={
-          <div>
-            <Header />
-            <Footer/>  
-          </div>}>
-          <Route index element={<App />} />
-
-          <Route
-            path="/half-sleeve-cut-and-sew-solid"
-            element={
-              <ProductCategoryHomePage
-                products={halfSleeveCutTShirts}
-                // products will be used to create product cards
-                productType={"half-sleeve-cut-and-sew-solid"}
-                // productType is used to create product url
-              />
-            }
-          />
-
-          <Route
-            path="/half-sleeve-cut-and-sew-solid/product"
-            element={<Product />}
-          >
-            <Route
-              path=":name"
-              element={<ProductPage getProductData={getHalfSleeveCutTShirt} />}
+        <Route path="/" element={<App />} />
+        <Route
+          path="/half-sleeve-cut-and-sew-solid"
+          element={
+            <ProductCategoryHomePage
+              products={halfSleeveCutTShirts}
+              productType={"half-sleeve-cut-and-sew-solid"}
             />
-          </Route>
-
-          <Route
-            path="/half-sleeve-dawah-tshirts-for-men"
-            element={
-              <ProductCategoryHomePage
-                products={getAllHalfSleeveDawaTShirtsData()}
-                // products will be used to create product cards
-                productType={"half-sleeve-dawah-tshirts-for-men"}
-                // productType is used to create product url
-              />
-            }
-          />
-
-          <Route
-            path="/half-sleeve-dawah-tshirts-for-men/product"
-            element={<Product />}
-          >
-            <Route
-              path=":name"
-              element={
-                <ProductPage
-                  getProductData={getSpecificHalfSleeveDawaTShirtData}
-                />
-              }
+          }
+        />
+        <Route
+          path="/half-sleeve-cut-and-sew-solid/product/:name"
+          element={<ProductPage getProductData={getHalfSleeveCutTShirt} />}
+        />
+        <Route
+          path="/half-sleeve-dawah-tshirts-for-men"
+          element={
+            <ProductCategoryHomePage
+              products={getAllHalfSleeveDawaTShirtsData()}
+              productType={"half-sleeve-dawah-tshirts-for-men"}
             />
-          </Route>
-
-          <Route
-            path="/half-sleeve-regular-tshirts-for-men"
-            element={
-              <ProductCategoryHomePage
-                products={getAllHalfSleeveRegularTShirtsData()}
-                // products will be used to create product cards
-                productType={"half-sleeve-regular-tshirts-for-men"}
-                // productType is used to create product url
-              />
-            }
-          />
-
-          <Route
-            path="/half-sleeve-regular-tshirts-for-men/product"
-            element={<Product />}
-          >
-            <Route
-              path=":name"
-              element={
-                <ProductPage
-                  getProductData={getSpecificHalfSleeveRegularTShirtData}
-                />
-              }
+          }
+        />
+        <Route
+          path="/half-sleeve-dawah-tshirts-for-men/product/:name"
+          element={
+            <ProductPage getProductData={getSpecificHalfSleeveDawaTShirtData} />
+          }
+        />
+        <Route
+          path="/half-sleeve-regular-tshirts-for-men"
+          element={
+            <ProductCategoryHomePage
+              products={getAllHalfSleeveRegularTShirtsData()}
+              productType={"half-sleeve-regular-tshirts-for-men"}
             />
-          </Route>
-
-          <Route
-            path="/polo-t-shirt"
-            element={
-              <ProductCategoryHomePage
-                products={getAllPoloTShirtsData()}
-                // products will be used to create product cards
-                productType={"polo-t-shirt"}
-                // productType is used to create product url
-              />
-            }
-          />
-
-          <Route path="/polo-t-shirt/product" element={<Product />}>
-            <Route
-              path=":name"
-              element={
-                <ProductPage getProductData={getSpecificPoloTShirtData} />
-              }
+          }
+        />
+        <Route
+          path="/half-sleeve-regular-tshirts-for-men/product/:name"
+          element={
+            <ProductPage
+              getProductData={getSpecificHalfSleeveRegularTShirtData}
             />
-          </Route>
-
-          <Route path="/cart" element={<Cart />} />
-          <Route
-            path="*"
-            element={
-              <h1 className="mt-40 flex max-w-full items-center justify-center font-bold text-gray-900">
-                Thanks for exploring the site. The page you are looking for is
-                not built yet.
-              </h1>
-            }
-          />
-        </Route>
+          }
+        />
+        <Route
+          path="/polo-t-shirt"
+          element={
+            <ProductCategoryHomePage
+              products={getAllPoloTShirtsData()}
+              productType={"polo-t-shirt"}
+            />
+          }
+        />
+        <Route
+          path="/polo-t-shirt/product/:name"
+          element={<ProductPage getProductData={getSpecificPoloTShirtData} />}
+        />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="*"
+          element={
+            <h1 className="mt-40 flex max-w-full items-center justify-center font-bold text-gray-900">
+              Thanks for exploring the site. The page you are looking for is not
+              built yet.
+            </h1>
+          }
+        />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
