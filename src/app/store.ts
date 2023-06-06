c625834af664fa4bbfe4406a8d701d6f21dd8f70
hattima-tim <irfanridwan20@gmail.com>
2023-06-06 03:31:06 +0000

    import productsInCartReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsInCartReducer from "../features/cart/productsInCartSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    productsInCart: productsInCartReducer,
+  },
 });
 
 export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
