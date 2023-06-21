a7a489cf598763e8040705a2a6ac29872c33fabf
hattima-tim <irfanridwan20@gmail.com>
2023-06-21 09:01:59 +0600

    change  import path

--- src/tests/halfSleeveHomePage/halfSleeveHomePage.test.tsx
+++ src/tests/halfSleeveHomePage/halfSleeveHomePage.test.tsx
@@ -1,5 +1,5 @@
 import { render,screen } from "@testing-library/react";
-import HalfSleeveHomePage from "../components/products/halfSleeveTShirts/main";
+import HalfSleeveHomePage from "../../components/products/halfSleeveTShirts/main";
 import {MemoryRouter} from "react-router-dom";
 
 test("renders with correct texts", () => {
